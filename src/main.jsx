c3d57becbbc80ff1c826a10f8a3cef799e8c9ac8
Nneif <nneifemakpunonu@gmail.com>
2024-01-08 19:04:47 -0500

    added the pages needed and working on the contact page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import Homepage from './pages/Homepage'
+import Products from './pages/Products'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />
+      },
+      {
+        path: '/homepage',
+        element: <Homepage />
+      },
+      {
+        path: '/products',
+        element: <Products />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { validateName, validateEmail, validateMessage } from '../utils/helpers';
+
+export default function Contact(){
+    return(
+        <>
+
+        </>
+    )
+}
